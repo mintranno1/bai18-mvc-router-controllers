719cfdb728ca054ea159cdc29ff2aa732b94c575
mintranno1 <minhtranhcm.mt@gmail.com>
2021-09-10 14:02:42 +0700

    them cau hinh router cho cac trang khong co nhieu router rieng le nhu home, about, contact -> tat ca cho vao cung 1 file -> routes -> site, app/controller/SiteController

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,13 @@
 const newsRouter = require('./news');
+const siteRouter = require('./site');
 
 function route(app){
-
-    // router default
-    app.get('/', (req, res) => {
-        res.render('home');
-    });
     
-    app.get('/search', (req, res) => {
-        
-        res.render('search');
-    });
-
     //nạp tat ca router lien quan den news từ -> news.js
     app.use('/news', newsRouter);
 
+    app.use('/', siteRouter);
+    
     
 }
 
